3331a20a4913e484d798cf77020ec824ce2ca4db
Thom <thomgusterson@gmail.com>
2020-05-15 15:55:31 +1200

    Updates

--- precache-manifest.3ff5de6cdda0a3a436149c3bebe5cfa9.js
+++ precache-manifest.3ff5de6cdda0a3a436149c3bebe5cfa9.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7c295a070180bf3a17a5d634a8e8abca",
+    "revision": "eee16050bff4210583be9ec42bdb0596",
     "url": "/React-Rock-Paper-Scissors/index.html"
   },
   {
